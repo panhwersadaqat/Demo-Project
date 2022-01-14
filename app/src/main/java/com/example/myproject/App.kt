e213b3e53c2b289cd2ebe74c4aac7e83872e4aa1
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2022-01-14 15:39:26 +0500

    initial commit

---
+++ app/src/main/java/com/example/myproject/App.kt
@@ -0,0 +1,12 @@
+package com.example.myproject
+
+import android.app.Application
+
+/**
+ * Created by Sadaqat Panhwer
+ * https://panhwersadaqat.github.io/
+ * on 1/14/22.
+ */
+
+class App : Application(){
+}
